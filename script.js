5983c57855c5dc2dc07fa5a0f0e4b7ddeb7d4806
Shallen-99 <shallenburks@gmail.com>
2025-11-11 19:58:40 -0500

    finished up the js

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ function add(task) {
     const status = document.getElementById("status").value
 
     if (!taskName || !category || !deadline) {
-        alert("Please Fill In ALl The Fields")
+        alert("Please Fill In All The Fields")
     }
 
     const newTask = {taskName, category, deadline, status}
@@ -44,6 +44,57 @@ function displayTasks() {
     const selSat = statusFilter.value
 
     const filteredTasks = tasks.filter(task => {
-        const 
+        const mCategory = selCat === "All" || task.category === selCat
+        const mStatus = selSat === "All" || task.status === selSat
+        return mCategory && mStatus
     })
+
+    filteredTasks.forEach((task, index) => {
+        const row = document.createElement("tr")
+        row.classList.toggle("overdue", task.status === "Overdue")
+        row.classList.toggle("completed", task.status === "Completed")
+
+        const select = document.createElement("select")
+        ["inProgress", "completed"].forEach(status => {
+            const option = document.creatElement("option")
+            option.value = status
+            option.textContent = status
+            if (task.status === status) option.selected = true
+            select.appendChild(option)
+        })
+
+        select.addEventListener("change", e => {
+            updateTaskStatus(index, e.target.value)
+        })
+
+        row.innerHTML = ` <td>${task.taskName}</td>
+        <td>${task.category}</td>
+        <td>${task.deadline}</td>
+        <td>${task.status}</td>
+        <td></td>`
+
+        row.lastElementChild.appendChild(select)
+        taskList.appendChild(row)
+    })
+    saveTasks()
 }
+
+function updateTaskStatus(index, newStatus) {
+   tasks[index].status = newStatus
+   saveTasks()
+   displayTasks() 
+}
+
+function updateCategoryFilter(){
+    const categories = ["All", ...new Set(tasks.map(task => task.category))]
+    categoryFilter.innerHTML = categories
+    .map(cat => `<option value = "${cat}">${cat}</option>`)
+    .join("")
+}
+
+taskForm.addEventListener("submit", add)
+categoryFilter.addEventListener("change", displayTasks)
+statusFilter.addEventListener("change", displayTasks)
+
+updateCategoryFilter()
+displayTasks()
